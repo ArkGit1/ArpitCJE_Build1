0b856de0b034467f668b36ec21386a7a8013eb33
ArkGit1 <45433352+ArkGit1@users.noreply.github.com>
2018-12-09 23:37:59 +0530

    Triggering first jenkins build commit

--- src/test/java/testScripts/RegularExpressionGroupTest.java
+++ src/test/java/testScripts/RegularExpressionGroupTest.java
@@ -7,6 +7,7 @@ public class RegularExpressionGroupTest {
 	 @Test(groups = { "include-test-one" })
 	    public void testMethodOne() {
 	        System.out.println("Test method one");
+	        System.out.println("First Jenkins Build Triggering");
 	    }
 	 
 	    @Test(groups = { "include-test-two" })
